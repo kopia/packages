cd8759ee0437b8aabb13527a8c4a06c634cac522
Jarek Kowalski <jaak@jkowalski.net>
2020-08-12 07:30:22 -0700

    testing

--- apt2/regen.sh
+++ apt2/regen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+cd dists/stable
 apt-ftparchive packages . > Packages
 gzip -k -f Packages
 apt-ftparchive release \
    -o APT::FTPArchive::Release::Architectures="amd64 arm64 armhf" \
+   -o APT::FTPArchive::Release::Distribution="stable" \
    . > Release
