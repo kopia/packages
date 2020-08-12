305ff7e56f67d250bae9bd3c362373cde7245820
Jarek Kowalski <jaak@jkowalski.net>
2020-08-12 08:12:11 -0700

    reorg

--- apt/regen.sh
+++ apt/regen.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-apt-ftparchive packages . > Packages
-gzip -k -f Packages
+for a in amd64 arm64 armhf; do
+   apt-ftparchive packages -a $a dists/stable/main/binary-$a > dists/stable/main/binary-$a/Packages
+   gzip -k -f dists/stable/main/binary-$a/Packages
+done
+
+cd dists/stable
+
 apt-ftparchive release \
    -o APT::FTPArchive::Release::Architectures="amd64 arm64 armhf" \
+   -o APT::FTPArchive::Release::Codename="stable" \
+   -o APT::FTPArchive::Release::Suite="stable" \
    . > Release
