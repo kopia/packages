77662ad134cc05b807f0ad63670e6f433b4e62e4
Jarek Kowalski <jaak@jkowalski.net>
2020-08-12 07:48:01 -0700

    testing

--- apt2/regen.sh
+++ apt2/regen.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cd dists/stable/main/binary-amd64
-apt-ftparchive packages -a amd64 . > Packages
-gzip -k -f Packages
-cd ../..
+cd dists/stable
+apt-ftparchive packages -a amd64 main/binary-amd64 > main/binary-amd64/Packages
+gzip -k -f main/binary-amd64/Packages
 apt-ftparchive release \
    -o APT::FTPArchive::Release::Architectures="amd64 arm64 armhf" \
    -o APT::FTPArchive::Release::Codename="stable" \
